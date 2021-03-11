8ae79db6ea8dfb8d1a5583d3869799d072649e38
Mohamed Abukar <abukar.mohamed@nokia.com>
2021-03-11 15:25:37 +0200

    Symptom data collection
    Change-Id: I4b90ddb786f3178706c2673940c6c419a90e2944
    Signed-off-by: Mohamed Abukar <abukar.mohamed@nokia.com>

--- pkg/restful/restful.go
+++ pkg/restful/restful.go
@@ -23,7 +23,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	//"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -76,7 +75,9 @@ func (r *Restful) Run() {
 
 	appmgr.Logger.Info("Xapp manager started ... serving on %s:%d\n", server.Host, server.Port)
 
+	go symptomdataServer()
 	go r.RetrieveApps()
+
 	if err := server.Serve(); err != nil {
 		log.Fatal(err.Error())
 	}
@@ -426,4 +427,26 @@ func (r *Restful) getAppConfig() (configList models.AllXappConfig) {
 
 	}
 	return
+}
+
+func (r *Restful) symptomdataServer() {
+	http.HandleFunc("/ric/v1/symptomdata", func(w http.ResponseWriter, req *http.Request) {
+		xappData := struct {
+			XappList 			models.AllDeployedXapps `json:"xappList"`
+			ConfigList 			models.AllXappConfig	`json:"configList"`
+			SubscriptionList	models.AllSubscriptions	`json:"subscriptionList"`
+		}{
+			GetApps(),
+			getAppConfig(),
+			r.rh.GetAllSubscriptions(),
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Content-Disposition", "attachment; filename=platform/apps_info.json")
+		w.WriteHeader(http.StatusOK)
+		resp, _ := json.MarshalIndent(xappData, "", "    ")
+		w.Write(string(resp))
+	})
+
+	http.ListenAndServe(":8081", nil)
 }
